bb987214cdfb063465051289004b41eb5d36ae72
Esteban Raffo <e.fraffo@gmail.com>
2020-07-30 22:13:05 -0300

    Updates

---
+++ precache-manifest.1ba471d71c068185a4d4baabe5079fb1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1c1397033979e7a4360ac183b8bc82fd",
+    "url": "/ghpages-test/index.html"
+  },
+  {
+    "revision": "9f706b636c48a4bde622",
+    "url": "/ghpages-test/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "c700b6415a2f4161c1ba",
+    "url": "/ghpages-test/static/js/2.6897c4cb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ghpages-test/static/js/2.6897c4cb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9f706b636c48a4bde622",
+    "url": "/ghpages-test/static/js/main.aaeca535.chunk.js"
+  },
+  {
+    "revision": "fed5b09d39e30cb957d9",
+    "url": "/ghpages-test/static/js/runtime-main.0e640570.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/ghpages-test/static/media/logo.5d5d9eef.svg"
+  }
+]);
